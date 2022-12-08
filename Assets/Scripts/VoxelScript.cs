891e075f0b948124bacde46d1500e908d1933684
pandang11 <zaq21216@naver.com>
2022-12-08 18:02:56 +0900

    add function(meteo all delete)
    <변경사항>
    1. 메테오 모두 삭제하는 아이템 추가
    2. 아이템 텍스처와 소리 추가
    3. 복셀 코드 통합

--- Assets/Scripts/VoxelScript.cs
+++ Assets/Scripts/VoxelScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hp_script : MonoBehaviour
+public class VoxelScript : MonoBehaviour
 {
     public float time = 1;
     public float distance = 5.0f;
